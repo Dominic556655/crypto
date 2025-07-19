4f40099c9eca5b1be5c23180493db47407eb145b
Dominic556655 <ichadominic5@gmail.com>
2025-07-19 14:36:05 +0100

    Update settings.py

--- crypto/settings.py
+++ crypto/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'fallback_dev_key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://crypto-blockchain-o70n.onrender.com']
 
 
 # Application definition
